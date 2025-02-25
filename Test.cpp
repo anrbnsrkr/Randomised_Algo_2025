ded3be0c384fd8d2337affabdd110610ea69c7e4
anrbnsrkr <89860477+anrbnsrkr@users.noreply.github.com>
2025-02-26 00:47:26 +0530

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -13,8 +13,11 @@
 using namespace std;
 
 int main() {
-    vector<long int> arr = random_list_of_size<long int>(50,-1, 0, 100);
-    vector<vector<long int>> z = n_permute_list<long int>(arr,3,-1);
+    vector<long int> arr = random_list_of_size<long int>(50,-1, 0, 100); // (Size, seed, LowerLimit, UpperLimit) // UL - LL must be >= size else it will fail // -1 for random seed
+    //overloaded function 
+    //random_list_of_size(size) returns random_list_of_size<long int>(size,18749,LONG_MIN,LONG_MAX)
+    //Can be used for repeatability. I made it for my testing purposes
+    vector<vector<long int>> z = n_permute_list<long int>(arr,3,-1); // (array, number of permuted array you want, seed) // same use seed = -1 for random seed
     int count = 0;
     cout<<"The array is: ";
     for(auto i : arr) {
